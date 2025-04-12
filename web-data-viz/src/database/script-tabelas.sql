51cea61650155ff656a0c9516815c27df89296ae
KaduhRibeiro1 <carlos.ribeiro@sptech.school>
2025-04-12 20:07:25 -0300

    backend login e cadastro

---
+++ web-data-viz/src/database/script-tabelas.sql
@@ -0,0 +1,15 @@
+create database beyond_analytics;
+
+use beyond_analytics;
+
+CREATE TABLE usuarios (
+    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    empresa varchar(100) UNIQUE NOT NULL,  
+    nome VARCHAR(100), 
+    email VARCHAR(100) UNIQUE NOT NULL, 
+    senha VARCHAR(255) NOT NULL
+);
+
+select * from usuarios;
+
+DROP DATABASE beyond_analytics;
---
+++ web-data-viz/src/models/usuarioModel.js
@@ -0,0 +1,27 @@
+var database = require("../database/config");
+
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function autenticar(): ", email, senha);
+    var instrucaoSql = `
+        SELECT idUsuario, empresa, nome, email FROM usuarios WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function cadastrar(empresa, nome, email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, empresa);
+    
+    // Inserção dos dados na tabela usuarios
+    var instrucaoSql = `
+        INSERT INTO usuarios (empresa, nome, email, senha) 
+        VALUES ('${empresa}', '${nome}', '${email}', '${senha}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    autenticar,
+    cadastrar
+};
