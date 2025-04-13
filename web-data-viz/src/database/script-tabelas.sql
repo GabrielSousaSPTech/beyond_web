14d12e1add59853c91d37025a8d7ce378d371ce7
AndreiScafiSPTech <andrei.vasconcelos@sptech.school>
2025-04-13 17:45:14 -0300

    Adicionando funcionalidade ao Cadastro e Login com Node.js, porém o código ainda precisa de refatoração.

---
+++ web-data-viz/src/database/script-tabelas.sql
@@ -0,0 +1,30 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql server
+*/
+
+CREATE DATABASE beyond;
+
+USE beyond;
+
+CREATE TABLE empresa (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	razao_social VARCHAR(50),
+	cnpj CHAR(14),
+	codigo_ativacao VARCHAR(50)
+);
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+	fk_empresa INT,
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+);
+
+
+insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
