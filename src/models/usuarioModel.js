c1bfb313b42732d5ca3304e2d55f2c38f638b676
fillipeMendesC <fillipe.coelho@sptech.school>
2025-05-07 11:27:13 -0300

    fix cadastro e userModel

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config");
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function autenticar(): ", email, senha);
     var instrucaoSql = `
-        SELECT idUsuario, empresa, nome, email FROM usuarios WHERE email = ? AND senha = ?;
+        SELECT ID_FUNC, FK_EMPRESA, NOME, EMAIL FROM TB_FUNCIONARIO WHERE email = ? AND senha = ?;
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql, [email, senha]);
@@ -13,7 +13,7 @@ function cadastrar(email, senha, empresa, nome, telefone) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", email, senha, empresa, nome, telefone);
     
     var instrucaoSql = `
-        INSERT INTO usuarios (empresa, nome, email, senha, telefone) 
+        INSERT INTO TB_FUNCIONARIO (FK_EMPRESA, NOME, EMAIL, SENHA, TEL) 
         VALUES (?, ?, ?, ?, ?);
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
