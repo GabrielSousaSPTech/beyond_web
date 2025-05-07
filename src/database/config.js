d13f0af3dafd82b056809c6d869d291d5bb231bf
fillipeMendesC <fillipe.coelho@sptech.school>
2025-05-07 13:56:24 -0300

    fix cadastro e continuação debug config

--- src/database/config.js
+++ src/database/config.js
@@ -10,13 +10,12 @@ var mySqlConfig = {
 };
 
 function executar(instrucao) {
+    onsole.log("Config: "+mySqlConfig+"==============================")
 
     if (process.env.AMBIENTE_PROCESSO !== "producao" && process.env.AMBIENTE_PROCESSO !== "desenvolvimento") {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM .env OU dev.env OU app.js\n");
         return Promise.reject("AMBIENTE NÃO CONFIGURADO EM .env");
     }
-
-    console.log("Config: "+mySqlConfig+"==============================")
 
     return new Promise(function (resolve, reject) {
         var conexao = mysql.createConnection(mySqlConfig);
