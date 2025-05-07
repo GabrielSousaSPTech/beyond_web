ac4c73217f7c070efa32df62f7e344c45b460d41
fillipeMendesC <fillipe.coelho@sptech.school>
2025-05-07 13:39:42 -0300

    debug mysql config

--- src/database/config.js
+++ src/database/config.js
@@ -17,6 +17,7 @@ function executar(instrucao) {
     }
 
     return new Promise(function (resolve, reject) {
+        console.log(mySqlConfig)
         var conexao = mysql.createConnection(mySqlConfig);
         conexao.connect();
         conexao.query(instrucao, function (erro, resultados) {
