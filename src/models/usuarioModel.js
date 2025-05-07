9354bc08d5252e2bed0ab605f0ffddcfe9e1602e
fillipeMendesC <fillipe.coelho@sptech.school>
2025-05-07 16:00:51 -0300

    fix pouco

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -24,7 +24,7 @@ function confirmarCodigo(codigo) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function confirmarCodigo():", codigo);
     
     var instrucaoSql = `
-        SELECT ID_EMPRESA FROM TB_EMPRESA WHERE ID_EMPRESA = ${codigo};
+        SELECT ID_EMPRESA FROM TB_EMPRESA WHERE CHAVE_ATIVACAO = ${codigo};
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
