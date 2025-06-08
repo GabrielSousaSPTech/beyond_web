043c14e4aecc6e65841bc602c2ab63cd03071ba4
Pedro <pedro.lima@sptech.school>
2025-06-08 11:45:57 -0300

    tela finalizada

--- project/app/app.routes.ts
+++ project/app/app.routes.ts
@@ -51,7 +51,7 @@ export const routes: Routes = [
         path: 'heatmap',
         pathMatch: 'full',
         loadComponent() {
-            return import('./modules/dashboard/heatmap/heatmap.component').then(
+            return import('./modules/dashboard/heatmap-brazil/heatmap.component').then(
                 m => m.HeatmapComponent
             );
         },
